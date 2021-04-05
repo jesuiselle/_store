a9dcab29848b82f778571fa195adbc3942f3b9f8
Oumayma Dhehibi <oumayma.dhehibi@esprit.tn>
2021-04-05 20:51:16 +0200

    ties between marketing and container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
-    return <h1>Hi</h1>;
+    return (
+        <div>
+            <MarketingApp />
+        </div>
+    );
 };
-
