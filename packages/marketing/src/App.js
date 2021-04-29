b94aaadd671ee694419211e4e9568807cba16edc
Oumayma Dhehibi <oumayma.dhehibi@esprit.tn>
2021-04-29 14:48:58 +0200

    adjust navigation

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import {Switch, Route, Router} from "react-router-dom";
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
@@ -9,15 +9,17 @@ const generateClassName = createGenerateClassName({
     productionPrefix: 'ma'
 })
 
-export default () => {
-    return <div>
-        <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/pricing" component={Pricing} />
-                    <Route path='/' component={Landing} />
-                </Switch>
-            </BrowserRouter>
-        </StylesProvider>
-    </div>
+export default ({history}) => {
+    return (
+        <div>
+            <StylesProvider generateClassName={generateClassName}>
+                <Router history={history}>
+                    <Switch>
+                        <Route exact path="/pricing" component={Pricing}/>
+                        <Route path='/' component={Landing}/>
+                    </Switch>
+                </Router>
+            </StylesProvider>
+        </div>
+    )
 }
